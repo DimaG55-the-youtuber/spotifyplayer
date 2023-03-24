c85764c3cac3e229149151a92ff7d2f90a899013
dimag55-the-youtuber <dimgolv@gmail.com>
2023-03-24 16:48:52 -0400

    SHH

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
-
 directory = 'songs/'
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    songs = [{"name": filename.name, "path": filename.path} for filename in os.scandir(directory) if filename.is_file()]
-    for i in songs:
-        uh = i["name"].replace('[SPOTIFY-DOWNLOADER.COM]', '_')
-        uh = uh.replace('.mp3', '')
-        uh = uh.replace('_', ' ')
-        songs.remove(i)
-        songs.append({"name": uh, "path": i["path"]})
+    songs = [{"name": filename.name.replace(".mp3", ""), "path": filename.path} for filename in os.scandir(directory) if filename.is_file()]
     return render_template('index.html', songs=songs)
