b446a7793926b883f37a9fecf485546a7777c51d
dimag55-the-youtuber <dimgolv@gmail.com>
2023-03-25 10:22:44 -0400

    ge

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 directory = 'songs/'
+link = "https://github.com/DimaG55-the-youtuber/spotifyplayer/blob/master/songs/"
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    songs = [{"name": filename.name.replace(".mp3", ""), "path": filename.path} for filename in os.scandir(directory) if filename.is_file()]
+    songs = [{"name": filename.name.replace(".mp3", ""), "path": f"{link}{filename.name}?raw=true"} for filename in os.scandir(directory) if filename.is_file()]
     return render_template('index.html', songs=songs)
